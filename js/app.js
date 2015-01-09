ed7361f3b95227c779ffd04bf49785dec636b8df
Petar Genov <genov1824@gmail.com>
2015-01-10 00:31:02 +0200

    add get adds

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var app = angular.module("adsApp", [ngRoute]);
+var app = angular.module("adsApp", ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
         controller: 'HomeCtrl'
     })
-}])
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+}]);
