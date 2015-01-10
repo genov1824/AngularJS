71535e4408ccdce8fc55e1cfa29652eca081d26e
Petar Genov <genov1824@gmail.com>
2015-01-10 14:36:42 +0200

    add login and register controllers

--- js/app.js
+++ js/app.js
@@ -6,7 +6,15 @@ app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
         controller: 'HomeCtrl'
-    })
+    });
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    });
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+    });
     $routeProvider.otherwise({
         redirectTo: '/'
     });
